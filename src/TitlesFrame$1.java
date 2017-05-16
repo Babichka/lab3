96ddd861f4f5f2d607cb1b4120f4496174db42b6
Babichka <melesa6@gmail.com>
2017-05-16 11:04:43 +0300

    Init commit with decompiled sources

---
+++ src/TitlesFrame$1.java
@@ -0,0 +1,34 @@
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ 
+/*    */ class TitlesFrame$1
+/*    */   implements Runnable
+/*    */ {
+/*    */   public void run()
+/*    */   {
+/* 25 */     TitlesFrame ps = new TitlesFrame();
+/* 26 */     ps.setVisible(true);
+/*    */   }
+/*    */ }
+
+
+/* Location:              /home/shok/Desktop/support/ex_2_decompile/Olesya/lab3/lab3.jar!/TitlesFrame$1.class
+ * Java compiler version: 7 (51.0)
+ * JD-Core Version:       0.7.1
+ */
